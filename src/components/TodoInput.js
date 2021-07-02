32f3145fff0f4e05cff8f2397526ec23e8dd0cd5
juhyoju <20125182@hanmail.net>
2021-07-02 16:45:55 +0900

    [add] get localStorage data list

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,44 +1,48 @@
-import React, {useState } from "react";
+import React, { useState, useEffect } from "react";
 import "style/TodoInput.css";
 
 const TodoInput = () => {
     const [todos, setTodos ] = useState([]);
     const [newTodo, setNewTodo] = useState("");
-    //const [check, setCheck] = useState(false);
     
-    const saveData = (newTodos) => {
-        window.localStorage.setItem("todos", JSON.stringify(newTodos));
-    };
-    
-    // useEffect(() => {
-    //     if (window.localStorage.getItem("todos")) {
-    //         setTodos(JSON.parse(localStorage.getItem("todos")));
-    //     }
-    // }, []);
-
     const onChange = (event) => {  
         setNewTodo(event.target.value);
     };
 
-    const onAddTodo = () => {
-        if (newTodo.trim()) {
-            const newTodos = [...todos, { todo: newTodo.trim(), id: Date.now() }];
-            setTodos(newTodos);
-            setNewTodo("");
-            saveData(newTodos);
+    const onSaveTodo = (newTodo) => {
+        window.localStorage.setItem("todoArray", JSON.stringify(newTodo));
+    };   
+
+    const onGetTodo = window.localStorage.getItem("todoArray");
+    useEffect(() => {
+        if (onGetTodo) {
+            setTodos(JSON.parse(onGetTodo));
         }
+    }, []);
+
+    const onAddTodo = () => {
+        const todoAll = [...todos, {todo: newTodo}];
+        setTodos(todoAll);
+        setNewTodo("");
+        onSaveTodo(todoAll);
     };
 
     return (
-        <div className="form">
-            <input
-                type="text"
-                value={newTodo}
-                onChange={onChange}
-                placeholder="things to do"
-            />
-            <button onClick={onAddTodo} className="add_btn">저장하기</button>
-        </div>
+        <>
+            <div className="form">
+                <input
+                    type="text"
+                    value={newTodo}
+                    onChange={onChange}
+                    placeholder="things to do"
+                />
+                <button onClick={onAddTodo} className="add_btn">저장하기</button>
+            </div>
+            <div style={{margin:50}}>{onGetTodo}</div>
+            
+            <button onClick={onDeleteTodo} className="add_btn">전체 삭제</button>
+
+        </>
     );
 };
 
