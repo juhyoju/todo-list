eb64a46d2c7513331cfe3ddde712a96a2e5ff6d5
hj <20125182@hanmail.net>
2021-07-13 18:15:17 +0900

    Implement each 'Todo' with 'index'

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,19 @@
 import React from "react";
+import "style/TodoList.css";
 
 const TodoList = ({todos}) => {
 
+    const todoResult = todos.map((oneTodo, index) => 
+        <li key={index}>
+            {oneTodo.todo}
+        </li>
+    );
+
     return (
         <>
-        <div>{todos.map((oneTodo) => 
-            (oneTodo.todo)
-        )}</div>
+       <div className="todoList">
+           <ul>{todoResult}</ul>
+       </div>
         </>
     )
 };
