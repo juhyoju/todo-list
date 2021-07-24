0aa2982d1b9a0e8fe8b5cf005fcb90f3783e13ca
hj <20125182@hanmail.net>
2021-07-24 15:46:26 +0900

    [add] delete todo

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -8,11 +8,14 @@ import TodoList from "./TodoList";
 const TodoTemplate = () => {
     // <span>UI(타이틀, children 값으로 내부에 추가할 컴포넌트)</span>
 
+    // 가져오기
     const onGetTodo = window.localStorage.getItem("todoArray");
     const [todos, setTodos] = useState(JSON.parse(onGetTodo)??[]);
     
     const onDeleteTodo = id => { 
-        setTodos(todos.filter(todos => todos.id !== id));
+        const onDeleteTodos = todos.filter(todos => todos.id !== id);
+        setTodos(onDeleteTodos);
+        window.localStorage.setItem("todoArray", JSON.stringify(onDeleteTodos));
     };
 
     return ( 
