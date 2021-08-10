2f925173e23488263c87ca5558dba1e22c59047f
juhyoju <20125182@hanmail.net>
2021-08-10 17:28:31 +0900

    [wip] drag & drop

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,36 @@
 import React from "react";
 import "style/TodoList.css";
 import TodoItem from "./TodoItem";
-
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 const TodoList = ({moveCard, todos, onDelete, onUpdateStatus}) => {    
     return (
         <>
         <div className="todoList">
-            <ul>
-            {todos.map(todo => (
-                <TodoItem 
-                    todo={todo} 
-                    key={todos.id} 
-                    onDelete={onDelete} 
-                    onUpdateStatus={onUpdateStatus} 
-                    moveCard={moveCard}
-                />
-            ))}
-            </ul>            
+            <DragDropContext>
+                <Droppable droppableId="todos">
+                    {(provided) => (
+                        <ul {...provided.droppableProps} ref={provided.innerRef}>
+                        {todos.map(todo => (
+                            <Draggable>
+                                {(provided) => (
+                                <TodoItem 
+                                    todo={todo} 
+                                    key={todos.id} 
+                                    onDelete={onDelete} 
+                                    onUpdateStatus={onUpdateStatus} 
+                                    moveCard={moveCard}
+                                    ref={provided.innerRef}
+                                    {...provided.dragHandleProps}
+                                    {...provided.draggableProps}
+                                />
+                            )}
+                            </Draggable>
+                        ))}
+                        </ul>
+                    )}
+                </Droppable>
+            </DragDropContext>         
         </div>
         </>
     )
