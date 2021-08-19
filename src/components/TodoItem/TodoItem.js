c3a7ed721146378736226e3734fce27f1cb8f59b
juhyoju <20125182@hanmail.net>
2021-08-19 17:32:20 +0900

    todoItem style

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes, faEdit } from '@fortawesome/free-solid-svg-icons'
+import styled from 'styled-components'
 
 import useTodoItem from './useTodoItem'
 
@@ -14,7 +15,7 @@ const TodoItem = ({
     const { onIsDoneItem, onDeleteTodoItem } = useTodoItem(todos, setTodos)
 
     return (
-        <li
+        <TodoList
             className={isDone ? 'done' : 'ing'}
             ref={ref}
             isDragging={isDragging}
@@ -38,7 +39,14 @@ const TodoItem = ({
                     <FontAwesomeIcon icon={faTimes} />
                 </button>
             </span>
-        </li>
+        </TodoList>
     )
 }
 export default TodoItem
+
+const TodoList = styled.li`
+    border: 1px solid #ddd;
+    padding: 25px;
+    box-shadow: 3px 3px 0 #eee;
+    margin-bottom: 15px;
+`
