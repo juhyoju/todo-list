70169ad71e4e78b00b13e94c6e2d4a76faa6d18c
juhyoju <20125182@hanmail.net>
2021-09-23 11:37:31 +0900

    코드 일부 수정

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import 'index.css'
 
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-
 ReactDOM.render(
-    <DndProvider backend={HTML5Backend}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </DndProvider>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+
     document.getElementById('root')
 )
